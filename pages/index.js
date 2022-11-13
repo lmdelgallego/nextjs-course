39459dfdf8d069d31f04c09dabafaab0a6eae8b3
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2022-11-12 22:58:00 -0500

    🏁 First Commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import { useRef } from "react";
+
+function HomePage() {
+
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+
+  }
+
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <form>
+        <div>
+          <label htmlFor="email">Your email address</label>
+          <input type="email" id="id" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" row="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button>Send Feedback</button>
+      </form>
+    </div>
+  );
+}
+
+export default HomePage;
