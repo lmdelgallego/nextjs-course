1f0e299d76290d8aac483effa47c0ad978a9a563
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-01-02 12:35:09 -0500

    feat(ConnectMongo): Connecting MongoDB

--- pages/api/newsletter.js
+++ pages/api/newsletter.js
@@ -1,12 +1,21 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+const uri = `mongodb+srv://admin:${process.env.MONGO_PASSWORD}@cluster0.onpg1w3.mongodb.net/?retryWrites=true&w=majority`;
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     const userEmail = req.body.email;
+    console.log(process.env.MONGO_PASSWORD);
 
     if (!userEmail || !userEmail.includes('@')) {
       res.status(422).json({ message: "Invalid email address." });
       return;
     }
-    console.log(userEmail);
+
+    const client = await MongoClient.connect(uri, { useUnifiedTopology: true })
+    const db = client.db();
+    await db.collection('emails').insertOne({ email: userEmail });
+    client.close();
     res.status(201).json({ message: "Signed up!" })
   }
 }
