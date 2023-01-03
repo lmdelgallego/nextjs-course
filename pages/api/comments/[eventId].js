28df1b5885f10f0f028ad779b5d6813661bc9f16
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-01-03 07:43:43 -0500

    feat(ConnectMongo): Improvement, get comment for a specific event

--- pages/api/comments/[eventId].js
+++ pages/api/comments/[eventId].js
@@ -53,7 +53,7 @@ async function handler(req, res) {
 
   if (req.method === 'GET') {
     try {
-      const documents = getAllDocuments(client, 'comments', { _id: -1 });
+      const documents = getAllDocuments(client, 'comments', { _id: -1 }, { eventId: eventId });
       res.status(201).json({ comments: documents });
     } catch (error) {
       res.status(500).json({ message: "Getting comments failed!" });
