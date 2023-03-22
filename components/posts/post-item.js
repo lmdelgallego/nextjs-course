952fc40cde0572f94c28ceee715ca6e2aba620c0
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-03-21 22:00:07 -0500

    👷🏽‍♂️ Feature post markup

---
+++ components/posts/post-item.js
@@ -0,0 +1,44 @@
+import Link from "next/link";
+import Image from "next/Image";
+import classes from './post-item.module.css';
+
+const PostItem = (props) => {
+
+  const {
+    title,
+    image,
+    excerpt,
+    date,
+    slug,
+  } = props.post;
+
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
+  const formattedDate = new Date(date).toLocaleString(
+    'en-US',
+    {
+      day: 'numeric',
+      month: 'long',
+      year: 'numeric',
+    }
+  );
+
+  return (
+    <li className={classes.post}>
+      <Link href={linkPath}>
+        <a>
+          <div className={classes.image}>
+            <Image src={imagePath} alt={title} width={300} height={200} layout="responsive"/>
+          </div>
+          <div className={classes.content}>
+            <h3>{title}</h3>
+            <time>{formattedDate}</time>
+            <p>{excerpt}</p>
+          </div>
+        </a>
+      </Link>
+    </li>
+  )
+}
+
+export default PostItem
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,38 @@
 import { Fragment } from "react"
 import Hero from "../components/home-page/hero"
+import FeaturePosts from "../components/home-page/featured-posts"
+
+const DUMMY_POSTS = [{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs',
+},{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs1',
+},
+{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs2',
+},{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs3',
+}];
 
 function HomePage() {
   return <Fragment>
     <Hero />
-    {/* <FeaturePosts /> */}
+    <FeaturePosts posts={DUMMY_POSTS}/>
   </Fragment>
 }
 
