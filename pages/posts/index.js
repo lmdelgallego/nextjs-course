ba039ce1371751609c4f80ccb253a3481f50f77d
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-06-21 15:59:04 -0500

    feat(BlogProject): Adding the "All Posts" Page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,34 @@
+import AllPosts from "../../components/posts/all-post"
+
+const DUMMY_POSTS = [{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs',
+},{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs1',
+},
+{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs2',
+},{
+  title: 'Getting started with Nextjs',
+  image: 'getting-started-nextjs.png',
+  excerpt:'Nextjs is a the React framework form production',
+  date: '2023-02-10',
+  slug: 'getting-started-with-nextjs3',
+}];
+
 function AllPostPage() {
-  return <div>Welcome to Post Page!</div>
+  return <AllPosts posts={DUMMY_POSTS} />
 }
 
 export default AllPostPage
