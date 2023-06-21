f29acfda7d4e6ad3210a25769ec4ded6c8e8e471
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-06-21 16:22:02 -0500

    feat(BlogProject): Rendering Markdown As JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,4 @@
+import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from './post-content.module.css'
 
@@ -13,7 +14,9 @@ function PostContent () {
   const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
   return <article className={classes.content}>
     <PostHeader title={DUMMY_POST.title} image={imagePath}/>
-    {DUMMY_POST.content}
+    <ReactMarkdown>
+      {DUMMY_POST.content}
+    </ReactMarkdown>
   </article>
 }
 
