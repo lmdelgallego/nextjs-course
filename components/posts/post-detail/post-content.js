90340029511955f04a575d5832acd9537b85564b
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-06-22 12:37:36 -0500

    feat(BlogProject): Rendering Dynamic Post Pages & Paths

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,20 +2,12 @@ import ReactMarkdown from "react-markdown";
 import PostHeader from "./post-header";
 import classes from './post-content.module.css'
 
-const DUMMY_POST = {
-  title: 'Getting started with Nextjs',
-  image: 'getting-started-nextjs.png',
-  date: '2023-02-10',
-  slug: 'getting-started-with-nextjs2',
-  content: '# This is a first post'
-}
-
-function PostContent () {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`
+function PostContent(props) {
+  const imagePath = `/images/posts/${props.post.slug}/${props.post.image}`
   return <article className={classes.content}>
-    <PostHeader title={DUMMY_POST.title} image={imagePath}/>
+    <PostHeader title={props.post.title} image={imagePath} />
     <ReactMarkdown>
-      {DUMMY_POST.content}
+      {props.post.content}
     </ReactMarkdown>
   </article>
 }
--- utils/post-util.js
+++ utils/post-util.js
@@ -1,33 +1,45 @@
 import fs from 'fs';
 import path from 'path';
+
 import matter from 'gray-matter';
 
-const postDirectory = path.join(process.cwd(), 'posts');
+const postsDirectory = path.join(process.cwd(), 'posts');
 
-function getPostData (fileName) {
-  const filePath = path.join(postDirectory, fileName)
+export function getPostsFiles() {
+  return fs.readdirSync(postsDirectory);
+}
+
+export function getPostData(postIdentifier) {
+  const postSlug = postIdentifier.replace(/\.md$/, ''); // removes the file extension
+  const filePath = path.join(postsDirectory, `${postSlug}.md`);
   const fileContent = fs.readFileSync(filePath, 'utf-8');
   const { data, content } = matter(fileContent);
-
-  const postSlug = fileName.replace(/\.md$/, ''); // remove the file extension
 
   const postData = {
     slug: postSlug,
     ...data,
-    content
-  }
+    content,
+  };
+
   return postData;
 }
 
-export function getAllPosts () {
-  const postFiles = fs.readdirSync(postDirectory);
-  const allPosts = postFiles.map(postFile => getPostData(postFile))
-  const sortedPosts = allPosts.sort( (postA, postB) => postA.date > postB.date ? -1 : 1 )
+export function getAllPosts() {
+  const postFiles = getPostsFiles();
+
+  const allPosts = postFiles.map(postFile => {
+    return getPostData(postFile);
+  });
+
+  const sortedPosts = allPosts.sort((postA, postB) => postA.date > postB.date ? -1 : 1);
+
   return sortedPosts;
 }
 
 export function getFeaturePosts() {
-  const allPosts = getAllPosts()
-  const featurePosts = allPosts.filter(post => post.isFeatured )
-  return featurePosts;
+  const allPosts = getAllPosts();
+
+  const featuredPosts = allPosts.filter(post => post.isFeatured);
+
+  return featuredPosts;
 }
