b0be2cd86f14cdc60118df5cbd8914b8e0736635
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-06-26 21:01:24 -0500

    feat(BlogProject): Storing Messages With MongoDB In A Database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler (req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler (req, res) {
 
   if ( req.method === 'POST' ) {
       const { email, name, message } = req.body;
@@ -21,7 +23,27 @@ function handler (req, res) {
       message
     }
 
-    console.log(newMessage);
+    let client
+
+    try {
+      client = await MongoClient.connect(process.env.MONGO_URL)
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+      return;
+    }
+
+    const db = client.db('my-site');
+
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing message failed!' });
+      return;
+    }
+
+    client.close();
 
     res.status(201).json({ message: 'Successfully stored message!', body: newMessage });
 
