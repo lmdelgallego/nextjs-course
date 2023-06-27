33e63588179c1705f61815ed77dee392041c92fb
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-06-26 21:22:17 -0500

    feat(BlogProject): Adding UI Feedback With Notifications

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,33 +1,99 @@
+import { useState, useEffect } from 'react';
+import Notification from '../ui/notification';
 import classes from './contact-form.module.css';
-import {useState} from 'react';
+
+async function sendContactData(contactDetail) {
+  const response = await fetch('/api/contact', {
+      method: 'POST',
+      body: JSON.stringify(contactDetail),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || 'Something went wrong!');
+    }
+
+    return data;
+}
 
 function ContactForm() {
 
   const [enteredEmail, setEnteredEmail] = useState('')
   const [enteredName, setEnteredName] = useState('')
   const [enteredMessage, setEnteredMessage] = useState('')
+  const [requestStatus, setRequestStatus] = useState(); // 'pending', 'success', 'error'
+  const [requestError, setRequestError] = useState();
 
-  function sendMessageHandler (event) {
+  useEffect(() => {
+    if(
+      requestStatus === 'success' ||
+      requestStatus === 'error'
+    ) {
+      const timer = setTimeout(() => {
+        setRequestStatus(null)
+        setRequestError(null)
+      }, 3000);
+
+      return () => clearTimeout(timer)
+    }
+  }, [requestStatus])
+
+
+  async function sendMessageHandler (event) {
     event.preventDefault();
-
+    setRequestStatus('pending');
     // optional: add client-side validation
     if (!enteredEmail ||!enteredName ||!enteredMessage){
       return
     }
-    console.log('sending message')
 
-    fetch('/api/contact', {
-      method: 'POST',
-      body: JSON.stringify({
+    try {
+      await sendContactData({
         email: enteredEmail,
         name: enteredName,
         message: enteredMessage
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
+      });
+      setRequestStatus('success');
+      setEnteredMessage('');
+      setEnteredEmail('');
+      setEnteredName('');
+
+    } catch (error) {
+      setRequestError(error.message);
+      setRequestStatus('error');
+    }
   }
+
+  let notification;
+
+  if (requestStatus === 'pending') {
+    notification = {
+      status: 'pending',
+      title: 'Sending message...',
+      message: 'Your message is on its way!'
+    }
+  }
+
+  if (requestStatus ==='success') {
+    notification = {
+      status:'success',
+      title: 'Success!',
+      message: `Thank You! Your request has been sent.`
+    }
+  }
+
+  if (requestStatus ==='error') {
+    notification = {
+      status:'error',
+      title: 'Error!',
+      message: `Your request could not be sent. ${requestError}`
+    }
+  }
+
 
   return (
     <section className={classes.contact}>
@@ -61,6 +127,13 @@ function ContactForm() {
           </div>
         </div>
       </form>
+      { notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
     </section>
   )
 }
