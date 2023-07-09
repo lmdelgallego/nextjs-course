d89e6d47f921fde5e3e57c795d7dca857e644f78
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-09 12:57:20 -0500

    feat(Auth): Adding the "Credentials Auth Provider" & User Login Logic

---
+++ pages/api/auth/[..nextauth].js
@@ -0,0 +1,36 @@
+import NextAuth from "next-auth";
+import Providers from "next-auth/providers";
+
+import { connectToDataBase, findDocument } from "../../../lib/db";
+import { verifyPassword } from "../../../lib/auth";
+
+export default NextAuth({
+  session: {
+    jwt: true
+  },
+  providers: [
+    Providers.Credentials({
+      authorize: async (credentials) => {
+        const client = connectToDataBase();
+        const user = findDocument(client, 'user', { email: credentials.email });
+
+        if (!user) {
+          client.close();
+          throw new Error('No user found!');
+        }
+
+        const isValid = await verifyPassword(credentials.password, user.password);
+
+        if (!isValid) {
+          client.close();
+          throw new Error('Could not log you in!');
+        }
+
+        client.close();
+
+        return { email: user.email };
+
+      }
+    })
+  ]
+});
