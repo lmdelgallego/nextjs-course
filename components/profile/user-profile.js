ebcc7b5259fe30fd264ef6d282e77813b012a627
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-09 16:51:57 -0500

    feat(Auth): Adding Server-Side Page Guards (And When To Use Which Approach)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,26 +1,26 @@
-import { getSession } from 'next-auth/client';
+// import { getSession } from 'next-auth/client';
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 
 function UserProfile() {
-  const [isLoading, setIsLoading] = useState(true);
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then(session => {
+  // useEffect(() => {
+  //   getSession().then(session => {
 
-      if (!session) {
-        window.location.href = '/auth';
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
 
   return (
     <section className={classes.profile}>
