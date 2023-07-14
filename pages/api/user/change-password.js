2e8f2e6887f6cda5b1c9538174e841926269e774
Luis Miguel Del Gallego Horta <lmdelgallego@gmail.com>
2023-07-14 13:11:33 -0500

    feat(Auth): Adding the "Change Password" Logic

--- pages/api/user/change-password.js
+++ pages/api/user/change-password.js
@@ -1,4 +1,6 @@
 import { getSession } from 'next-auth/client';
+import { connectToDataBase, findDocument, updateDocument } from '../../../lib/db';
+import { verifyPassword, hashPassword } from '../../../lib/auth';
 
 async function handler(req, res) {
   if (req.method !== 'PATCH') {
@@ -12,6 +14,43 @@ async function handler(req, res) {
     return;
   }
 
+  const userEmail = session.user.email;
+  const { newPassword, oldPassword } = req.body;
+
+  const client = await connectToDataBase();
+  const user = await findDocument(client, 'users', { email: userEmail });
+
+  if (!user) {
+    res.status(404).json({ message: 'User not found' });
+    client.close();
+    return;
+  }
+
+  const currentPassword = user.password;
+
+  const passwordsAreEqual = await verifyPassword(oldPassword, currentPassword);
+
+  if (passwordsAreEqual) {
+    res.status(403).json({ message: 'Invalid password' });
+    client.close();
+    return;
+  }
+
+  const hashedPassword = await hashPassword(newPassword);
+
+  const result = await updateDocument(client, 'users',
+    { email: userEmail },
+    { $set: { password: hashedPassword } }
+  );
+
+  if (result.modifiedCount === 0) {
+    res.status(500).json({ message: 'Could not update password.' });
+    client.close();
+    return;
+  }
+
+  client.close();
+  res.status(200).json({ message: 'Password updated!' });
 }
 
 export default handler;
